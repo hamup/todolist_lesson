46e2741839de494ce9188d3f6b71371d454266d2
hamu <gc.kai.g@gmail.com>
2018-01-23 00:28:27 +0900

    fitst commit

---
+++ src/main/kotlin/com/example/todolist/TaskController.kt
@@ -0,0 +1,22 @@
+package com.example.todolist
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("tasks")
+class TaskController {
+
+    @GetMapping("")
+    fun index(model: Model): String {
+        val tasks = listOf(
+                Task(1, "障子を張り替える", false),
+                Task(2, "定期検診に行く", true),
+                Task(3, "公共料金を払う", false)
+        )
+        model.addAttribute("tasks",tasks)
+        return "tasks/index"
+    }
+}
