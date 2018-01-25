67536b302df49c6a22fa6ca1676073f1daf9db86
hamu <gc.kai.g@gmail.com>
2018-01-26 01:10:34 +0900

    タスク更新機能の追加

--- src/main/kotlin/com/example/todolist/TaskUpdateForm.kt
+++ src/main/kotlin/com/example/todolist/TaskUpdateForm.kt
@@ -3,11 +3,9 @@ package com.example.todolist
 import org.hibernate.validator.constraints.NotBlank
 import javax.validation.constraints.Size
 
-class TaskCreateForm {
+class TaskUpdateForm {
 
-    //入力必須項目
     @NotBlank
-    //20文字以内に制限
     @Size(max = 20)
     var content: String? = null
 
